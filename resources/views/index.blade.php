e558d37c9cf9cc3e44f5251f1e752f873a75255c
Naufaldi Egaputra R <faldiega@gmail.com>
2019-10-16 11:35:53 +0700

    update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
   <div class="row">
     <div class="col-10">
-<h1 class="mt-3">Hello, world!</h1>
+<h1 class="mt-3">Hello, world! Saya sedang belajar laravel</h1>
     </div>
   </div>
 </div>
